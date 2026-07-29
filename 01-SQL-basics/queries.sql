167a07daef0f58d77110b5895d5f793db96ce8ca
Vidhi Binani <vidhibinani34@gmail.com>
2026-07-29 19:59:48 +0530

    Add SQL scripts for College, Company, and Library DBs
    Added SQL scripts to create databases and tables for College, Company, and Library.

--- 01-SQL-basics/queries.sql
+++ 01-SQL-basics/queries.sql
@@ -1,1 +1,55 @@
+-- =========================================================
+-- Topic: SQL Basics
+-- Concepts: CREATE DATABASE, USE, CREATE TABLE, Constraints
+-- =========================================================
 
+
+-- =========================================================
+-- Practice 1 : College Database
+-- =========================================================
+
+CREATE DATABASE CollegeDB;
+
+USE CollegeDB;
+
+CREATE TABLE Students(
+    studentid INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    age INT,
+    status VARCHAR(50) DEFAULT 'Active'
+);
+
+
+-- =========================================================
+-- Practice 2 : Company Database
+-- =========================================================
+
+CREATE DATABASE CompanyDB;
+
+USE CompanyDB;
+
+CREATE TABLE Employees(
+    employeeid INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    salary INT,
+    email VARCHAR(50) UNIQUE,
+    joiningDate DATE
+);
+
+
+-- =========================================================
+-- Practice 3 : Library Database
+-- =========================================================
+
+CREATE DATABASE LibraryDB;
+
+USE LibraryDB;
+
+CREATE TABLE Books(
+    bookid INT PRIMARY KEY AUTO_INCREMENT,
+    author VARCHAR(50) NOT NULL,
+    issuedby VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    issueDate DATE
+);
